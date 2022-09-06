81076fc68441dcd77ccc4b79c0f3970ad0341095
Kipp Dunn <kipp.dunn@infernored.com>
2022-09-06 11:04:51 -0400

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Kipp Dunn on 9/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
